43b9ad8bf3dc2ba4a0ce1f7c100d6a4a41c75b47
scuroworks <scuro@protonmail.com>
2016-07-10 19:00:06 +0200

    Use non-google host for tests, prevent duplicate test runs

--- tests/test_fetch_source.py
+++ tests/test_fetch_source.py
@@ -5,12 +5,12 @@ from qisbot import fetch_source
 class TestFetchSource(unittest.TestCase):
 
     def test_fetch_source_positive(self):
-        source = fetch_source('http://brainjar.com/java/host/test.html')
+        source = fetch_source('https://httpbin.org/')
         self.assertIsNotNone(source)
         self.assertIn('<html>', source)
 
     def test_fetch_source_invalid_url(self):
-        self.assertIsNone(fetch_source('https://localhost/'))
+        self.assertIsNone(fetch_source('inva://listhost.xyz/'))
 
     def test_fetch_source_no_url(self):
         self.assertIsNone(fetch_source(None))
--- tests/test_select.py
+++ tests/test_select.py
@@ -6,21 +6,21 @@ from qisbot import select_first
 class TestSelect(unittest.TestCase):
 
     def test_select_positive(self):
-        result = select('https://google.com/', '//*[@id="hplogo"]')
+        result = select('https://httpbin.org/', '//*[@id="ENDPOINTS"]')
         self.assertTrue(result)
-        self.assertEqual(result[0].get('title'), 'Google')
+        self.assertEqual(result[0].text, 'ENDPOINTS')
 
     def test_select_invalid_host(self):
-        self.assertIsNone(select('https://localhost/', '//*[@id="hplogo"]'))
+        self.assertIsNone(select('inva://lidpage.com', '//*[@id="hplogo"]'))
 
     def test_select_invalid_selector(self):
-        self.assertIsNone(select('https://bing.com/', '?!2145'))
+        self.assertIsNone(select('https://httpbin.org/', '?!2145'))
 
     def test_select_empty_selector(self):
-        self.assertIsNone(select('https://bing.com/', ''))
+        self.assertIsNone(select('https://httpbin.org/', ''))
 
     def test_select_no_selector(self):
-        self.assertIsNone(select('https://bing.com/', None))
+        self.assertIsNone(select('https://httpbin.org/', None))
 
 
 class TestSelectFirst(unittest.TestCase):
