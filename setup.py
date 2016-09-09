a658b442cfbf061c3b3ec789f2ce0da5f8bba298
scuroworks <scuro@protonmail.com>
2016-09-09 16:54:34 +0200

    Clear all the things

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
     name='qisbot',
-    version='0.1',
+    version='0.9',
     author='scuroworks',
     author_email='scuro@protonmail.com',
     url='https://github.com/scuroworks/qisbot',
     license='GPLv3',
-    scripts=['qisbot.py'],
+    packages=['qisbot'],
 )
