f1cd2db844ca3af50cb63994b46c25644cec691e
scuroworks <scuro@protonmail.com>
2016-09-17 17:04:01 +0200

    Basic argument parsing for config- & db file paths

--- runbot.py
+++ runbot.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
+import argparse
+
 from qisbot.bot import Bot
 
 
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', '-c', type=str, default='./qisbot.ini', help='Path to the configuration file')
+    parser.add_argument('--database', '-d', type=str, default='./qisbot.db', help='Path to the database file')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    bot = Bot(config_path='sample_config.ini', database_path='sample_database.db')
+    arguments = parse_arguments()
+    bot = Bot(config_path=getattr(arguments, 'config'), database_path=getattr(arguments, 'database'))
