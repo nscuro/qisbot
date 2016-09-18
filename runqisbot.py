04d52fb53783b9a42732a69336eafaa4dba6e455
scuroworks <scuro@protonmail.com>
2016-09-18 21:50:46 +0200

    Integrate basic commands in command line arguments

--- runqisbot.py
+++ runqisbot.py
@@ -9,9 +9,19 @@ def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', '-c', type=str, default='./qisbot.ini', help='Path to the configuration file')
     parser.add_argument('--database', '-d', type=str, default='./qisbot.db', help='Path to the database file')
+    parser.add_argument('--print', '-p', default=False, action='store_true',
+                        help='Print the exams extract as table')
+    parser.add_argument('--force-refresh', '-f', default=False, action='store_true',
+                        help='Force a refresh of exams extract')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     arguments = parse_arguments()
     bot = Bot(config_path=getattr(arguments, 'config'), database_path=getattr(arguments, 'database'))
+    if getattr(arguments, 'print'):
+        bot.print_exams_extract(force_refresh=getattr(arguments, 'force_refresh'))
+    elif getattr(arguments, 'force_refresh'):
+        # This will just perform any actions provided by subscribers of new/changed exam events
+        bot.refresh_exams_extract()
+
