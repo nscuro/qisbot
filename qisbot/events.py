647dab0f0e53b8e253631f2745726e32073db14d
scuroworks <scuro@protonmail.com>
2016-09-18 19:07:07 +0200

    Introduce basic event system using zope.event

---
+++ qisbot/events.py
@@ -0,0 +1,64 @@
+import functools
+
+import zope.event
+
+from qisbot import models
+
+
+class BaseEvent(object):
+    """Base class for all Events."""
+    pass
+
+
+class NewExamEvent(BaseEvent):
+    """Event that notifies about new Exams."""
+
+    def __init__(self, exam: models.Exam):
+        self.exam = exam
+
+
+class ExamChangedEvent(BaseEvent):
+    """Event that notifies about changes in Exams."""
+
+    def __init__(self, old_exam: models.Exam, new_exam: models.Exam):
+        self.old_exam = old_exam
+        self.new_exam = new_exam
+
+
+def subscriber_of(event_type: BaseEvent):
+    """Restrict the decorated subscriber function to the given Event."""
+
+    def decorator(func):
+        @functools.wraps(func)
+        def verify_event(*args, **kwargs):
+            event = args[0]
+            if not isinstance(event, event_type):
+                return
+            return func(*args, **kwargs)
+
+        return verify_event
+
+    return decorator
+
+
+@subscriber_of(NewExamEvent)
+def on_new_exam_stdout(event: NewExamEvent) -> ():
+    """Subscriber to NewExamEvent that prints to stdout."""
+    pass
+
+
+@subscriber_of(ExamChangedEvent)
+def on_exam_changed_stdout(event: ExamChangedEvent) -> ():
+    """Subscriber of ExamChangedEvent that prints to stdout."""
+    pass
+
+
+@subscriber_of(ExamChangedEvent)
+def on_exam_changed_email(event: ExamChangedEvent) -> ():
+    """Subscriber to ExamChangedEvent that notifies the user via E-Mail."""
+    pass
+
+
+# Register all default subscribers
+zope.event.subscribers.append(on_new_exam_stdout)
+zope.event.subscribers.append(on_exam_changed_stdout)
