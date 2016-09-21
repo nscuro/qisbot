1a3348b2957464247b662a308f3a3fdb33fe6011
scuroworks <scuro@protonmail.com>
2016-09-21 20:50:58 +0200

    Minor improvements for mail notifications

--- qisbot/notifies/email.py
+++ qisbot/notifies/email.py
@@ -1,3 +1,4 @@
+import random
 import typing
 import smtplib
 import email.mime.text
@@ -8,6 +9,25 @@ import zope.event.classhandler
 from qisbot import models
 from qisbot import events
 from qisbot import config
+
+_emotions = [
+    'happy',
+    'pumped',
+    'excited',
+    'thrilled',
+    'pleased'
+]
+
+_actions = [
+    'inform you',
+    'let you know',
+]
+
+_new_exam_message = ('qisbot is {emotion} to {action} that the result of your "{exam}" exam '
+                     'has just been published.\nYou may want to check it out yourself, but I '
+                     'can show you what I know about it:\n')
+_updated_exam_message = ('qisbot is {emotion} to {action} that your "{exam}" exam has just\n'
+                         'been updated! I noticed the following changes:\n')
 
 
 @contextmanager
@@ -32,8 +52,9 @@ def _email_connection(conf: config.QisConfiguration) -> typing.Union[smtplib.SMT
 
 @zope.event.classhandler.handler(events.NewExamEvent)
 def on_new_exam_email(event: events.NewExamEvent) -> ():
-    message_content = """qisbot is excited to inform you that the result of your \"{}\" exam
-    has just been published! Here is everything I know about it:\n\n""".format(event.exam.name)
+    """Notify the user about a newly published exam result via E-Mail."""
+    message_content = _new_exam_message.format(emotion=random.choice(_emotions), action=random.choice(_actions),
+                                               exam=event.exam.name)
     for attr_name in models.ExamData.__members__.keys():
         attribute = getattr(event.exam, attr_name)
         if attribute and attribute != 'None':
@@ -47,8 +68,9 @@ def on_new_exam_email(event: events.NewExamEvent) -> ():
 
 @zope.event.classhandler.handler(events.ExamChangedEvent)
 def on_exam_changed_email(event: events.ExamChangedEvent) -> ():
-    message_content = """qisbot is happy to let you know that your \"{}\" exam was just updated!
-        \nThe following changes have been made:\n""".format(event.old_exam.name)
+    """Notify the user about an updated exam result via E-Mail."""
+    message_content = _updated_exam_message.format(emotion=random.choice(_emotions), action=random.choice(_actions),
+                                                   exam=event.old_exam.name)
     for changed_attr, values in event.changes.items():
         message_content += '\t- {}: "{}" -> "{}"\n'.format(changed_attr, values[0], values[1])
     message = email.mime.text.MIMEText(message_content, _subtype='plain')
