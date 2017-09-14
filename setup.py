078a4b8587c29e1199f40b3c4497cbaba4cbe15f
nscuro <scuro@protonmail.com>
2017-09-14 11:00:13 +0200

    Update author info

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 setup(
     name='qisbot',
     version='0.9',
-    author='scuroworks',
+    author='nscuro',
     author_email='scuro@protonmail.com',
-    url='https://github.com/scuroworks/qisbot',
+    url='https://github.com/nscuro/qisbot',
     license='GPLv3',
     packages=['qisbot'],
     scripts=['runqisbot']
